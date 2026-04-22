5f4f1d579f96cd68c3a85c00ee575eefb73ef973
Valery-Rosero <128109786+Valery-Rosero@users.noreply.github.com>
2026-04-22 01:32:30 -0500

    Eliminación de cosas

--- OneDrive/Documentos/Electiva I/tutorialVehiculosDjango/GestionVehiculos/AppVehiculos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TareaAPIView
-
-urlpatterns = [
-
-    path('', TareaAPIView.as_view(), name='tarea-list'),  
-    path('<int:pk>/', TareaAPIView.as_view(), name='tarea-detail'),  
-]
